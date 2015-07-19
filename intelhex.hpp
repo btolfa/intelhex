2def7834618b937c5b8b9485006e33f8d1947679
Tengiz Sharafiev btolfa@gmail.com <btolfa@gmail.com>
2015-07-19 14:41:31 +0300

    Initial source and CMakeLists.txt

---
+++ intelhex.hpp
@@ -0,0 +1,21 @@
+/** @brief Class to manipulate data from Intel hex file
+* @author Tengiz Sharafiev <btolfa@gmail.com>
+* @date 18.07.2015
+*/
+
+#pragma once
+
+#include <string>
+
+namespace ih {
+
+class intelhex {
+public:
+    intelhex(std::string const& path) {
+    }
+private:
+
+};
+
+}
+
---
+++ tests/intelhexTest.cpp
@@ -0,0 +1,6 @@
+/**
+* @author Tengiz Sharafiev <btolfa@gmail.com>
+* @date 18.07.2015
+*/
+
+#include "intelhex.hpp"
