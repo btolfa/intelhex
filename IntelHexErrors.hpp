af108fc4d7b5be2dc836125704825059299debe1
Tengiz Sharafiev btolfa@gmail.com <btolfa@gmail.com>
2015-07-19 16:16:10 +0300

    add some forgotten exceptions

--- IntelHexErrors.hpp
+++ IntelHexErrors.hpp
@@ -1,5 +1,28 @@
 /** @brief Exception for intelhex class
  * base on http://pythonhosted.org/IntelHex/appendix-a.html
+ *
+ *
+    IntelHexError - base error
+        HexReaderError - general hex reader error
+            AddressOverlapError - data for the same address overlap
+            HexRecordError - hex record decoder base error
+                RecordLengthError - record has invalid length
+                RecordTypeError - record has invalid type (RECTYP)
+                RecordChecksumError - record checksum mismatch
+                EOFRecordError - invalid EOF record (type 01)
+                ExtendedAddressRecordError - extended address record base error
+                    ExtendedSegmentAddressRecordError - invalid extended segment address record (type 02)
+                    ExtendedLinearAddressRecordError - invalid extended linear address record (type 04)
+                StartAddressRecordError - start address record base error
+                    StartSegmentAddressRecordError - invalid start segment address record (type 03)
+                    StartLinearAddressRecordError - invalid start linear address record (type 05)
+                    DuplicateStartAddressRecordError - start address record appears twice
+                    InvalidStartAddressValueError - invalid value of start addr record
+        BadAccess16bit - not enough data to read 16 bit value
+        NotEnoughDataError - not enough data to read N contiguous bytes
+        EmptyIntelHexError - requested operation cannot be performed with empty object
+
+ *
 * @author Tengiz Sharafiev <btolfa@gmail.com>
 * @date 19.07.2015
 */
@@ -15,13 +38,6 @@ class IntelHexError : public std::runtime_error {
 public:
     virtual ~IntelHexError() = default;
     using std::runtime_error::runtime_error;
-};
-
-/// general hex reader error
-class HexReaderError : public IntelHexError {
-public:
-    virtual ~HexReaderError() = default;
-    using IntelHexError::IntelHexError;
 };
 
 /// not enough data to read 16 bit value
@@ -42,35 +58,55 @@ public:
         using IntelHexError::IntelHexError;
 };
 
-/// record has invalid length
-class RecordLengthError : public HexReaderError {
+/// general hex reader error
+class HexReaderError : public IntelHexError {
 public:
-        using HexReaderError::HexReaderError;
+    virtual ~HexReaderError() = default;
+    using IntelHexError::IntelHexError;
+};
+
+/// data for the same address overlap
+class AddressOverlapError : public HexReaderError {
+public:
+    using HexReaderError::HexReaderError;
+};
+
+/// hex record decoder base error
+class HexRecordError : public HexReaderError {
+public:
+    virtual ~HexRecordError() = default;
+    using HexReaderError::HexReaderError;
+};
+
+/// record has invalid length
+class RecordLengthError : public HexRecordError {
+public:
+        using HexRecordError::HexRecordError;
 };
 
 /// record has invalid type (RECTYP)
-class RecordTypeError : public HexReaderError {
+class RecordTypeError : public HexRecordError {
 public:
-        using HexReaderError::HexReaderError;
+        using HexRecordError::HexRecordError;
 };
 
 /// record checksum mismatch
-class RecordChecksumError : public HexReaderError {
+class RecordChecksumError : public HexRecordError {
 public:
-        using HexReaderError::HexReaderError;
+        using HexRecordError::HexRecordError;
 };
 
 /// invalid EOF record (type 01)
-class EOFRecordError : public HexReaderError {
+class EOFRecordError : public HexRecordError {
 public:
-        using HexReaderError::HexReaderError;
+        using HexRecordError::HexRecordError;
 };
 
 /// extended address record base error
-class ExtendedAddressRecordError : public HexReaderError {
+class ExtendedAddressRecordError : public HexRecordError {
 public:
         virtual ~ExtendedAddressRecordError() = default;
-        using HexReaderError::HexReaderError;
+        using HexRecordError::HexRecordError;
 };
 
 /// invalid extended segment address record (type 02)
@@ -86,10 +122,10 @@ public:
 };
 
 /// start address record base error
-class StartAddressRecordError : public HexReaderError {
+class StartAddressRecordError : public HexRecordError {
 public:
         virtual ~StartAddressRecordError() = default;
-        using HexReaderError::HexReaderError;
+        using HexRecordError::HexRecordError;
 };
 
 /// invalid start segment address record (type 03)
