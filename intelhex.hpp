9383b861e47f531c4bdcd52e0374ecf86c0fe71e
Tengiz Sharafiev btolfa@gmail.com <btolfa@gmail.com>
2015-07-19 17:00:57 +0300

    Tests for check correctness of line and decoding record from string to seq of bytes

--- intelhex.hpp
+++ intelhex.hpp
@@ -6,13 +6,72 @@
 #pragma once
 
 #include <string>
+#include <istream>
+#include <vector>
+
+#include <boost/range/algorithm/for_each.hpp>
+
+#include "IntelHexErrors.hpp"
 
 namespace intelhex {
 
 class IntelHex {
 public:
+    /// Построчно читаем содержимое потока в ihex
+    friend std::istream &operator>>(std::istream &istream, IntelHex &ihex) {
+        std::string line;
+        while (std::getline(istream, line)) {
+            ihex.checkCorrectnessOrThrow(line);
+            ihex.parseLine(line);
+        }
+        return istream;
+    }
 
+    void checkCorrectnessOrThrow(std::string const &line) const {
+        // Строка должна начинаться с :
+        if (line.front() != ':') {
+            throw HexRecordError("Decoding string should start with ':'");
+        }
+
+        // В строке должно быть нечётное количество символов : + по 2 символа на байт
+        if (!(line.size() % 2)) {
+            throw HexRecordError("Decoding string should have odd chars - : and even char for bytes");
+        }
+
+        // Проверяем что в строке только допустимые символы - :, 0-9, a-f, A-F
+        boost::for_each(line, [](const auto & ch){
+            if ((ch == ':') || ((ch >= '0') && (ch <= '9')) || ((ch >= 'a') && (ch <= 'f')) || ((ch >= 'A') && (ch <= 'F'))) {
+                throw HexRecordError("Decoding string have invalid char: " + ch);
+            }
+        });
+    }
+
+    std::uint8_t convertCharToByte(char ch) const {
+        if (ch >= '0' && ch <= '9') {
+            return static_cast<std::uint8_t>(ch - '0');
+        } else if (ch >= 'a' && ch <= 'f') {
+            return static_cast<std::uint8_t>(ch - 'a' + 0xA);
+        } else {
+            return static_cast<std::uint8_t>(ch - 'A' + 0xA);
+        }
+    }
+
+    template <typename Iterator>
+    std::uint8_t convertStrToByte(Iterator itr) const {
+        return (convertCharToByte(*itr) << 4) | convertCharToByte(*(itr+1));
+    }
+
+    std::vector <std::uint8_t> decodeRecord(const std::string &line) const {
+        std::vector<std::uint8_t> result;
+        for (auto itr = line.cbegin() + 1; itr != line.cend(); itr+=2) {
+            result.emplace_back(convertStrToByte(itr));
+        }
+        return result;
+    }
+
+    void parseLine(std::string const &line) {
+        auto record = decodeRecord(line);
+    }
 };
 
 }
-
--- tests/intelhexTest.cpp
+++ tests/intelhexTest.cpp
@@ -13,8 +13,25 @@ class IntelHexTest : public ::testing::Test {
 
 };
 
-TEST_F(IntelHexTest, ShouldOpenHexFileByPath) {
+/// Должен бросить иключение для некорректной строки
+TEST_F(IntelHexTest, ShouldThrowIfInvalidDecodingString) {
+    intelhex::IntelHex intelHex;
 
+    EXPECT_THROW(intelHex.checkCorrectnessOrThrow("00"),intelhex::HexRecordError);
+    EXPECT_THROW(intelHex.checkCorrectnessOrThrow(":0"),intelhex::HexRecordError);
+    EXPECT_THROW(intelHex.checkCorrectnessOrThrow(":?0"),intelhex::HexRecordError);
+}
+
+
+
+/** @brief Должен декодировать запись из строки в набор байт
+*/
+TEST_F(IntelHexTest, ShouldDecodeRecord) {
+    intelhex::IntelHex intelHex;
+
+    std::string line = ":00000001FF";
+    std::vector<std::uint8_t> expected {0x00,0x00, 0x00, 0x01, 0xFF};
+    EXPECT_THAT(intelHex.decodeRecord(line), ::testing::ContainerEq(expected));
 }
 
 }
